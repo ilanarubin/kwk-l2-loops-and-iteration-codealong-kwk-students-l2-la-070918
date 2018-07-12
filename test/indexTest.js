db5332c6327b058ec226d68ba6b1710832c2ce95
ilanarubin <ilanarubin007@gmail.com>
2018-07-12 22:41:10 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,36 +1,36 @@
 const expect = chai.expect;
 
-describe('index.js', () => {
-  describe('printBadges()', () => {
-    let spy;
+// describe('index.js', () => {
+//   describe('printBadges()', () => {
+//     let spy;
 
-    beforeEach(() => {
-      spy = sinon.spy(console, 'log');
-    });
+//     beforeEach(() => {
+//       spy = sinon.spy(console, 'log');
+//     });
 
-    afterEach(() => {
-      spy.restore();
-    });
+//     afterEach(() => {
+//       spy.restore();
+//     });
 
-    it('invokes console.log() once for each element in the passed-in array', () => {
-      printBadges(['Lisa', 'Kaitlin', 'Jan']);
+//     it('invokes console.log() once for each element in the passed-in array', () => {
+//       printBadges(['Lisa', 'Kaitlin', 'Jan']);
 
-      expect(spy.callCount).to.eq(3);
-    });
+//       expect(spy.callCount).to.eq(3);
+//     });
 
-    it('prints out a welcome badge for each employee', () => {
-      printBadges(['Joe', 'Gabe']);
+//     it('prints out a welcome badge for each employee', () => {
+//       printBadges(['Joe', 'Gabe']);
 
-      expect(spy.calledWithExactly('Welcome Joe! You are employee #1.')).to.be.true;
-      expect(spy.calledWithExactly('Welcome Gabe! You are employee #2.')).to.be.true;
-    });
+//       expect(spy.calledWithExactly('Welcome Joe! You are employee #1.')).to.be.true;
+//       expect(spy.calledWithExactly('Welcome Gabe! You are employee #2.')).to.be.true;
+//     });
 
-    it('returns the passed-in array', () => {
-      const arr = [];
+//     it('returns the passed-in array', () => {
+//       const arr = [];
 
-      expect(printBadges(arr)).to.eq(arr);
-    });
-  });
+//       expect(printBadges(arr)).to.eq(arr);
+//     });
+//   });
 
 //   describe('tailsNeverFails()', () => {
 //     let spy;
